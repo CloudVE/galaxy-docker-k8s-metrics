78673b91f91a5ef3a2ab2628da5eb61e34fc552b
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2019-12-04 19:38:34 +0530

    Added more metrics

--- scripts/gather-metrics.sh
+++ scripts/gather-metrics.sh
@@ -5,8 +5,21 @@ set -e
 
 data=$(mktemp --suffix .gxadmin)
 
-# Export metrics to file
-gxadmin meta slurp-current --date > "$data"
+# send output of all commands to file
+{
+  gxadmin iquery queue-overview
+  gxadmin iquery queue-detail
+  gxadmin iquery jobs-queued
+  gxadmin iquery upload-gb-in-past-hour
+  gxadmin iquery users-count
+  gxadmin iquery collection-usage
+  gxadmin iquery ts-repos
+  gxadmin iquery user-disk-usage
+  gxadmin iquery tool-errors
+  gxadmin iquery tool-likely-broken
+  # Export all server statistics
+  gxadmin meta slurp-current --date
+} > "$data"
 
 # Ship to influxdb
 gxadmin meta influx-post "$INFLUX_DATABASE" "$data"
